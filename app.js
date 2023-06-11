7c933edc4f541dab0fd10a6bf6aa9f3fc6e37fb9
codesejin <seijin0722@naver.com>
2023-06-11 19:10:11 +0900

    jwt토큰으로이 로그인

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const JWT = require("jsonwebtoken");
 const app = express();
 
-app.post('/login', function (req, res, next) {
-  const user = { // 사용자 정보
-    userId: 203, // 사용자의 고유 아이디 (Primary key)
-    email: "archepro84@gmail.com", // 사용자의 이메일
-    name: "이용우", // 사용자의 이름
+app.post('/login', async (req, res) => {
+  // 사용자 정보
+  const user = {
+    userId: 203,
+    email: "archepro84@gmail.com",
+    name: "이용우",
   }
 
-  res.cookie('sparta', user);  // sparta 라는 이름을 가진 쿠키에 user 객체를 할당합니다.
+  // 사용자 정보를 JWT로 생성
+  const userJWT = await JWT.sign(user, // user 변수의 데이터를 payload에 할당
+    "secretOrPrivateKey", // JWT의 비밀키를 secretOrPrivateKey라는 문자열로 할당
+    { expiresIn: "1h" } // JWT의 인증 만료시간을 1시간으로 설정
+  );
+
+  // userJWT 변수를 sparta 라는 이름을 가진 쿠키에 Bearer 토큰 형식으로 할당
+  res.cookie('sparta', `Bearer ${userJWT}`);
   return res.status(200).end();
 });
 
